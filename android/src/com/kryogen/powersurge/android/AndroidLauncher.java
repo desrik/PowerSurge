6b209b4b26f49223a9d6d38e2cb2162623d60220
Donnie McNabb <desrik.dagoth@gmail.com>
2015-12-14 22:49:39 -0500

    Initial Commit

---
+++ android/src/com/kryogen/powersurge/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kryogen.powersurge.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kryogen.powersurge.PowerSurge;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PowerSurge(), config);
+	}
+}
